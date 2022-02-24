f97bcea503ea3b2f59783ce832bd2863ba72f65e
AlejandroG10 <alejandrogilromero@fp.iesromerovargas.com>
2022-02-24 10:14:58 +0100

    Arreglado

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tgawj0ar7fm8!x^$kut9)b=p4zkxydtuxi^%ar%ifdt7qfj3u6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
